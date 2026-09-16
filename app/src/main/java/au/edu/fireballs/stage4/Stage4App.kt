73160505661623423abee7076dc20cb379f86edf
Lewis Lakerink <llakerink@swin.edu.au>
2026-09-16 14:24:01 +1000

    fix(offline): restore mapbox tile interceptor for imagery overlay

--- app/src/main/java/au/edu/fireballs/stage4/Stage4App.kt
+++ app/src/main/java/au/edu/fireballs/stage4/Stage4App.kt
@@ -5,8 +5,10 @@ import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.WorkManager
+import au.edu.fireballs.stage4.data.tiles.AuthenticatedTileHttpInterceptor
 import coil.Coil
 import coil.ImageLoader
+import com.mapbox.common.HttpServiceFactory
 import com.mapbox.common.MapboxOptions
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
@@ -21,6 +23,9 @@ class Stage4App :
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
+    @Inject
+    lateinit var tileHttpInterceptor: AuthenticatedTileHttpInterceptor
+
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder().setWorkerFactory(workerFactory).build()
 
@@ -31,6 +36,13 @@ class Stage4App :
             Log.e("Stage4App", "MAPBOX_TOKEN is blank; Mapbox cannot initialize")
         }
         MapboxOptions.accessToken = BuildConfig.MAPBOX_TOKEN
+        HttpServiceFactory.setHttpServiceInterceptor(tileHttpInterceptor)
+        tileHttpInterceptor.installCancellationCallback()
         Coil.setImageLoader(imageLoader)
+    }
+
+    override fun onTerminate() {
+        HttpServiceFactory.reset()
+        super.onTerminate()
     }
 }
